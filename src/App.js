822327935ba1e65a61dd34512aaedfc7d73aa983
Arnav <arnavsolanki511@gmail.com>
2023-02-27 00:36:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Maps from './Components/Maps/Maps';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { getData, getWeatherData } from './APIs/Index';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({ sw: {}, ne: {} });
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+  const [filteredPlaces, setFilteredPlaces] = useState([])
+  const [weatherData, setWeatherData] = useState([])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    });
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      getWeatherData(coordinates.lat, coordinates.lng)
+        .then((data) => setWeatherData(data));
+
+      getData(type, bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data && data.filter((place) => place.name && place.num_reviews > 0));
+          setFilteredPlaces([]);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [type, bounds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List Places={filteredPlaces.length ? filteredPlaces : places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Maps setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} Places={filteredPlaces.length ? filteredPlaces : places} setChildClicked={setChildClicked} weatherData={weatherData}/>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
