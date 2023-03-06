52871e5ff1a367bc9e4838c859e5352fdd74b16e
Arnav <arnavsolanki511@gmail.com>
2023-03-06 22:15:23 +0530

    approved

---
+++ src/Components/Header/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './newStyles.css'
+
+function header() {
+  return (
+    <div>
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default header
